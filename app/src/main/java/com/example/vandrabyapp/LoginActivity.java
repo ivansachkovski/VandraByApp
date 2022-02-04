f08809192ad3fb54099fd3ea31fdd00ff9a97c55
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2022-02-04 17:45:54 +0300

    UI: create main activity

--- app/src/main/java/com/example/vandrabyapp/LoginActivity.java
+++ app/src/main/java/com/example/vandrabyapp/LoginActivity.java
@@ -1,8 +1,9 @@
 package com.example.vandrabyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        openMainActivity();
+    }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
