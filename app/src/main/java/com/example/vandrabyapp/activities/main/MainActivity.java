3ffc213ac9152fb82d18a43f2a78b08f7b6efab2
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2022-02-04 20:18:41 +0300

    UI: create profile page

--- app/src/main/java/com/example/vandrabyapp/activities/main/MainActivity.java
+++ app/src/main/java/com/example/vandrabyapp/activities/main/MainActivity.java
@@ -3,14 +3,64 @@ package com.example.vandrabyapp.activities.main;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.vandrabyapp.R;
+import com.example.vandrabyapp.activities.main.pages.profile.ProfileFragment;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PROFILE_FRAGMENT_TAG = "ProfileFragment";
+    private static final String SWIPES_FRAGMENT_TAG = "SwipesFragment";
+    private static final String TRIPS_FRAGMENT_TAG = "TripsFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationBarView bottomNavigationBar = findViewById(R.id.bottom_navigation_panel);
+        bottomNavigationBar.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.item_swipes:
+                    return openSwipesPage();
+                case R.id.item_trips:
+                    return openTripsPage();
+                case R.id.item_profile:
+                    return openProfilePage();
+                default:
+                    return false;
+            }
+        });
+
+        // choose default screen
+        bottomNavigationBar.setSelectedItemId(R.id.item_profile);
+    }
+
+    private boolean openSwipesPage() {
+        return true;
+    }
+
+    private boolean openTripsPage() {
+        return true;
+    }
+
+    private boolean openProfilePage() {
+        loadFragment(ProfileFragment.newInstance(), PROFILE_FRAGMENT_TAG, true);
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment, String tag, boolean saveState) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.layout_content, fragment, tag);
+
+        if (saveState) {
+            fragmentTransaction = fragmentTransaction.addToBackStack(null);
+        }
+
+        fragmentTransaction.commit();
     }
 }
