77e120676419b9686b10c83d00c2f926fa7cb7a0
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2022-02-10 19:28:36 +0300

    Add 3 empty tabs on Profile screen

--- app/src/main/java/com/example/vandrabyapp/activities/main/pages/profile/ProfileFragment.java
+++ app/src/main/java/com/example/vandrabyapp/activities/main/pages/profile/ProfileFragment.java
@@ -7,10 +7,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.vandrabyapp.R;
+import com.example.vandrabyapp.activities.main.pages.profile.adapters.ProfileTabsStateAdapter;
 import com.example.vandrabyapp.model.database.DataModel;
 import com.example.vandrabyapp.model.entities.User;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class ProfileFragment extends Fragment {
 
@@ -32,6 +37,26 @@ public class ProfileFragment extends Fragment {
 
         TextView textUserNickname = view.findViewById(R.id.text_user_nickname);
         textUserNickname.setText(user.getNickname());
+
+        FragmentStateAdapter adapter = new ProfileTabsStateAdapter(this);
+
+        ViewPager2 viewPager = view.findViewById(R.id.view_pager);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = view.findViewById(R.id.layout_tabs);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Объекты");
+                    break;
+                case 1:
+                    tab.setText("Маршруты");
+                    break;
+                case 2:
+                    tab.setText("Статистика");
+                    break;
+            }
+        }).attach();
 
         return view;
     }
---
+++ app/src/main/java/com/example/vandrabyapp/activities/main/pages/profile/adapters/ProfileTabsStateAdapter.java
@@ -0,0 +1,101 @@
+package com.example.vandrabyapp.activities.main.pages.profile.adapters;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vandrabyapp.R;
+
+public class ProfileTabsStateAdapter extends androidx.viewpager2.adapter.FragmentStateAdapter {
+
+    private final static int NUMBER_OF_PAGES = 3;
+
+    public ProfileTabsStateAdapter(Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return LikedPlacesPage.newInstance();
+            case 1:
+                return TripsPage.newInstance();
+            case 2:
+                return StatisticPage.newInstance();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return NUMBER_OF_PAGES;
+    }
+
+    // TODO::pass arguments
+    public static class LikedPlacesPage extends Fragment {
+
+        public static LikedPlacesPage newInstance() {
+            return new LikedPlacesPage();
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            RecyclerView view = (RecyclerView) inflater.inflate(R.layout.list_item_profile_tab, null);
+            //view.setAdapter(new PlacesViewAdapter(DataModel.getInstance().getUserLikedPlaces(), placeDetailsPageListener, 0));
+            return view;
+        }
+    }
+
+    // TODO::pass arguments
+    static public class TripsPage extends Fragment {
+        public static TripsPage newInstance() {
+            return new TripsPage();
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            RecyclerView view = (RecyclerView) inflater.inflate(R.layout.list_item_profile_tab, null);
+            // TODO::implement
+            return view;
+        }
+    }
+
+    // TODO::pass arguments
+    static public class StatisticPage extends Fragment {
+        public static StatisticPage newInstance() {
+            return new StatisticPage();
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            RecyclerView view = (RecyclerView) inflater.inflate(R.layout.list_item_profile_tab, null);
+            // TODO::implement
+            return view;
+        }
+    }
+}
